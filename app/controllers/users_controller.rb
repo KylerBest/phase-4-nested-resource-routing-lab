b0fbb6ab577114db3801add904e7a81acfc0d33b
Kyler Best <kyler.b@live.com>
2022-10-31 13:52:41 -0500

    commit tree 4ae0a010219b80130e0777e7f989681b314fbe3b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,42 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_message
 
   def show
     user = User.find_by(id: params[:id])
     render json: user, include: :items
+  end
+
+  def item_index
+    items = Item.where(user_id: params[:user_id])
+    if(User.find(params[:user_id]))
+      render json: items
+    else
+      render_not_found_message
+    end
+  end
+
+  def item_show
+    item = Item.find_by(user_id: params[:user_id], id: params[:id])
+    if(item)
+      render json: item
+    else
+      render json: {error: "Item not found"}, status: :not_found
+    end
+  end
+
+  def item_create
+    if(User.find(params[:user_id]))
+      item = Item.create(name: params[:name], description: params[:description], price: params[:price], user_id: params[:user_id])
+      render json: item, status: :created
+    else
+      render_not_found_message
+    end
+  end
+
+  private
+
+  def render_not_found_message
+    render json: {error: "User not found"}, status: :not_found
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :items
+  get "/users/:user_id/items", to: "users#item_index"
+  get "/users/:user_id/items/:id", to: "users#item_show"
+  post "/users/:user_id/items", to: "users#item_create"
+  resources :users
 end
